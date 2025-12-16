5d57481baa0debc5d35b222274d9b517da9a7064
Alex McNurlin <alemcnurlin@gmail.com>
2025-12-15 20:59:52 -0700

    Configure jest to work with typescript

--- server/src/__test__/connectionsService.spec.ts
+++ server/src/__test__/connectionsService.spec.ts
@@ -7,7 +7,18 @@ test("can load driver pairings", () => {
       name: "first",
       teammates: ["2"],
     },
+    {
+      id: "2",
+      name: "second",
+      teammates: [],
+    },
   ]);
 
-  expect(actual.length).toBe(1);
+  expect(actual).toEqual([
+    {
+      driver1: "1",
+      driver2: "2",
+      dates: [{ start: 1234, end: 5678 }],
+    },
+  ]);
 });
