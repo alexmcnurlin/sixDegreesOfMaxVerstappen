900cf25eed4a7fe0c5f183a27960f2754bcb8a1e
Alex McNurlin <alemcnurlin@gmail.com>
2025-12-16 07:11:20 -0700

    Migrate the server to babel
    * The TSC compiler requires you put *.js on all your imports. Jest (using Babel) _won't_ allow you to put *.js on your imports. Since we're using babel for jest anyways, lets use that for the regular build process

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,50 +1,52 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema.js';
-import { ConnectionsService } from './connectionsService.js';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import { ConnectionsService } from "./connectionsService";
 
 console.log("Loading driver data...");
 const drivers = ConnectionsService.loadDriverData(null);
-console.log(`-> Loaded ${drivers.length} drivers!`)
-console.log("Loading driver pairings data...")
+console.log(`-> Loaded ${drivers.length} drivers!`);
+console.log("Loading driver pairings data...");
 const pairings = ConnectionsService.loadDriverPairings(drivers);
-console.log(`-> Loaded ${pairings.length} pairings!`)
+console.log(`-> Loaded ${pairings.length} pairings!`);
 
 const resolvers = {
-    Query: {
-        drivers() {
-            return drivers
-        },
-        degreesOfSeparation(parent, args, contextValue, info) {
-            // TODO: Get the real data
-            console.log(args)
-            return pairings.filter(p => args.driver1 === p.driver1 || args.driver === p.driver2);
-        },
+  Query: {
+    drivers() {
+      return drivers;
     },
-    Driver: {
-        teammates(parent) {
-            console.log("Sup")
-            // TODO: This is NOT efficient. Turn drivers into a dictionary, keyed by ID
-            return pairings.filter(p => parent.id === p.driver1);
-        },
+    degreesOfSeparation(parent, args, contextValue, info) {
+      // TODO: Get the real data
+      console.log(args);
+      return pairings.filter(
+        (p) => args.driver1 === p.driver1 || args.driver === p.driver2
+      );
     },
-    Pairing: {
-        driver1(parent) {
-            return drivers.find(d => d.id == parent.driver1);
-        },
-        driver2(parent) {
-            return drivers.find(d => d.id == parent.driver2);
-        }
-    }
+  },
+  Driver: {
+    teammates(parent) {
+      console.log("Sup");
+      // TODO: This is NOT efficient. Turn drivers into a dictionary, keyed by ID
+      return pairings.filter((p) => parent.id === p.driver1);
+    },
+  },
+  Pairing: {
+    driver1(parent) {
+      return drivers.find((d) => d.id == parent.driver1);
+    },
+    driver2(parent) {
+      return drivers.find((d) => d.id == parent.driver2);
+    },
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 5172 },
+startStandaloneServer(server, {
+  listen: { port: 5172 },
+}).then(({ url }) => {
+  console.log(`🚀  Server reay at: ${url}`);
 });
-
-console.log(`🚀  Server ready at: ${url}`);
