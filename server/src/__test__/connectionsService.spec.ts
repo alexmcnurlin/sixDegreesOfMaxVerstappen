ea397a16ee62f28657feca15b4d2d141726cedc6
Alex McNurlin <alemcnurlin@gmail.com>
2025-12-16 18:34:16 -0700

    Add implementation of Floyd-Warsall algorithm to get the path between any two drivers

--- server/src/__test__/connectionsService.spec.ts
+++ server/src/__test__/connectionsService.spec.ts
@@ -1,6 +1,6 @@
 import { ConnectionsService } from "../connectionsService";
 
-test("can load driver pairings", () => {
+test("can load one driver pairing", () => {
   const actual = ConnectionsService.loadDriverPairings([
     {
       id: "1",
@@ -18,7 +18,250 @@ test("can load driver pairings", () => {
     {
       driver1: "1",
       driver2: "2",
-      dates: [{ start: 1234, end: 5678 }],
+      dates: [],
+    },
+  ]);
+});
+
+test("can load multiple driver pairings for one driver", () => {
+  const actual = ConnectionsService.loadDriverPairings([
+    {
+      id: "1",
+      name: "first",
+      teammates: ["2", "3"],
+    },
+    {
+      id: "2",
+      name: "second",
+      teammates: [],
+    },
+    {
+      id: "3",
+      name: "third",
+      teammates: [],
+    },
+  ]);
+
+  expect(actual).toEqual([
+    {
+      driver1: "1",
+      driver2: "2",
+      dates: [],
+    },
+    {
+      driver1: "1",
+      driver2: "3",
+      dates: [],
+    },
+  ]);
+});
+
+test("can load multiple driver pairings for multiple drivers", () => {
+  const actual = ConnectionsService.loadDriverPairings([
+    {
+      id: "1",
+      name: "first",
+      teammates: ["2", "3"],
+    },
+    {
+      id: "2",
+      name: "second",
+      teammates: ["3"],
+    },
+    {
+      id: "3",
+      name: "third",
+      teammates: [],
+    },
+  ]);
+
+  expect(actual).toEqual([
+    {
+      driver1: "1",
+      driver2: "2",
+      dates: [],
+    },
+    {
+      driver1: "1",
+      driver2: "3",
+      dates: [],
+    },
+    {
+      driver1: "2",
+      driver2: "3",
+      dates: [],
+    },
+  ]);
+});
+
+test("Pairings are symmetric when the data is symmetric", () => {
+  const actual = ConnectionsService.loadDriverPairings([
+    {
+      id: "1",
+      name: "first",
+      teammates: ["2"],
+    },
+    {
+      id: "2",
+      name: "second",
+      teammates: ["1"],
+    },
+  ]);
+
+  expect(actual).toEqual([
+    {
+      driver1: "1",
+      driver2: "2",
+      dates: [],
+    },
+    {
+      driver1: "2",
+      driver2: "1",
+      dates: [],
+    },
+  ]);
+});
+
+test("getPath results are not symmetric when the data is not symmetric", () => {
+  // This is not necessarily desired behavior, but the algorithm we use to
+  // detect the path lets us do it.
+  const drivers = [
+    {
+      id: "1",
+      name: "first",
+      teammates: ["2"],
+    },
+    {
+      id: "2",
+      name: "second",
+      teammates: ["3"],
+    },
+    {
+      id: "3",
+      name: "third",
+      teammates: ["1"],
+    },
+  ];
+  const pairings = ConnectionsService.loadDriverPairings(drivers);
+  const getMap = ConnectionsService.buildDegreesOfSeparationMap(
+    drivers,
+    pairings
+  );
+
+  const expected1 = getMap("1", "2");
+  const expected2 = getMap("2", "1");
+
+  expect(expected1.length).toBe(1);
+  expect(expected2.length).toBe(2);
+});
+
+test("getPath lets you get the path between two drivers", () => {
+  const drivers = [
+    { id: "1", name: "first", teammates: ["2"] },
+    { id: "2", name: "second", teammates: ["1"] },
+  ];
+  const pairings = ConnectionsService.loadDriverPairings(drivers);
+  const getPath = ConnectionsService.buildDegreesOfSeparationMap(
+    drivers,
+    pairings
+  );
+
+  const path = getPath("1", "2");
+
+  expect(path).toEqual([
+    {
+      driver1: "1",
+      driver2: "2",
+      dates: [],
+    },
+  ]);
+});
+
+test("getPath will be in reverse for reverse order", () => {
+  const drivers = [
+    { id: "1", name: "first", teammates: ["2"] },
+    { id: "2", name: "second", teammates: ["1"] },
+  ];
+  const pairings = ConnectionsService.loadDriverPairings(drivers);
+  const getPath = ConnectionsService.buildDegreesOfSeparationMap(
+    drivers,
+    pairings
+  );
+
+  const path = getPath("2", "1");
+
+  expect(path).toEqual([
+    {
+      driver1: "2",
+      driver2: "1",
+      dates: [],
+    },
+  ]);
+});
+
+test("getPath returns empty array for same id", () => {
+  const drivers = [
+    { id: "1", name: "first", teammates: ["2"] },
+    { id: "2", name: "second", teammates: ["1"] },
+  ];
+  const pairings = ConnectionsService.loadDriverPairings(drivers);
+  const getPath = ConnectionsService.buildDegreesOfSeparationMap(
+    drivers,
+    pairings
+  );
+
+  const path = getPath("1", "1");
+
+  expect(path).toEqual([]);
+});
+
+test("getPath returns null if no path", () => {
+  const drivers = [
+    { id: "1", name: "first", teammates: [] },
+    { id: "2", name: "second", teammates: [] },
+  ];
+  const pairings = ConnectionsService.loadDriverPairings(drivers);
+  const getPath = ConnectionsService.buildDegreesOfSeparationMap(
+    drivers,
+    pairings
+  );
+
+  const path = getPath("1", "2");
+
+  expect(path).toBe(null);
+});
+
+test("getPath gets the path when we have many drivers", () => {
+  const drivers = [
+    { id: "1", name: "first", teammates: ["2"] },
+    { id: "2", name: "second", teammates: ["1", "3", "4"] },
+    { id: "3", name: "third", teammates: ["2", "4"] },
+    { id: "4", name: "fourth", teammates: ["2", "3", "5"] },
+    { id: "5", name: "fifth", teammates: ["5"] },
+  ];
+  const pairings = ConnectionsService.loadDriverPairings(drivers);
+  const getPath = ConnectionsService.buildDegreesOfSeparationMap(
+    drivers,
+    pairings
+  );
+
+  const path = getPath("1", "5");
+
+  expect(path).toEqual([
+    {
+      driver1: "1",
+      driver2: "2",
+      dates: [],
+    },
+    {
+      driver1: "2",
+      driver2: "4",
+      dates: [],
+    },
+    {
+      driver1: "4",
+      driver2: "5",
+      dates: [],
     },
   ]);
 });
--- server/src/connectionsService.ts
+++ server/src/connectionsService.ts
@@ -25,18 +25,87 @@ export class ConnectionsService {
     return drivers;
   }
 
+  /**
+   * Maps the driver data into an object that represents all teammate pairings
+   *
+   * Each pairing should show up twice (I.e. Max was teammates with Carlos, AND
+   * Carlos was teammates with Max), but only if that's reflected in the data.
+   */
   static loadDriverPairings(data: Driver[]): Pairing[] {
     return data.flatMap((d) =>
       d.teammates.map((tm) => ({
         driver1: d.id,
         driver2: tm,
         dates: [
-          {
-            start: 1234,
-            end: 5678,
-          },
+          // TODO: Fill this in
         ],
       }))
     );
+  }
+
+  /**
+   * Build an arrays that will map out the number of degrees between any two
+   * drivers, and the path between them.
+   *
+   * The map is built using the Floyd-Warshall algorithm with path
+   * reconstruction described here
+   * https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm#Path_reconstruction
+   */
+  static buildDegreesOfSeparationMap(drivers: Driver[], pairings: Pairing[]) {
+    const length = drivers.length;
+    const indexMap: Record<string, number> = {};
+    drivers.forEach(({ id }, i) => (indexMap[id] = i));
+    const degrees: number[][] = Array.from({ length: length }, () =>
+      Array.from({ length: length }, () => Infinity)
+    );
+    const pathMap: Pairing[][] = Array.from({ length: length }, () =>
+      Array.from({ length: length }, () => null)
+    );
+
+    // Initialze the weights
+    pairings.forEach((p) => {
+      const u = indexMap[p.driver1];
+      const v = indexMap[p.driver2];
+      console.log(`${u}, ${v}`);
+      degrees[u][v] = 1;
+      pathMap[u][v] = p;
+    });
+    drivers.forEach((d) => {
+      const v = indexMap[d.id];
+      degrees[v][v] = 0;
+    });
+
+    // Run the Floyd-Warshall algorithm
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        for (let k = 0; k < length; k++) {
+          if (degrees[i][j] > degrees[i][k] + degrees[k][j]) {
+            degrees[i][j] = degrees[i][k] + degrees[k][j];
+            pathMap[i][j] = pathMap[k][j];
+          }
+        }
+      }
+    }
+
+    const getPath = (start: string, end: string) => {
+      const u = indexMap[start];
+      let v = indexMap[end];
+      if (start == end) {
+        return [];
+      }
+      if (pathMap[u][v] == null) {
+        return null;
+      }
+      const path = [];
+      let prevIndex = v;
+      while (u != v) {
+        const p = pathMap[u][v];
+        path.unshift(p);
+        v = indexMap[p.driver1];
+      }
+      return path;
+    };
+
+    return getPath;
   }
 }
