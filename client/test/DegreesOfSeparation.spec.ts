ed37d2434668235317160fda25ba72cba5e09ff0
Alex McNurlin <alemcnurlin@gmail.com>
2025-12-28 14:56:42 -0700

    Add test file that I forgot to commit in a8d73ab8f2e21f10de8c7e7ac3b126e7dd0c4ef7

---
+++ client/test/DegreesOfSeparation.spec.ts
@@ -0,0 +1,168 @@
+import { test, expect } from "@playwright/test";
+import { interceptGql } from "./interceptGql";
+
+test("can see race details", async ({ page }) => {
+  // Arrange
+  await interceptGql(page, "GetDrivers", {
+    drivers: [
+      { id: "1", name: "Max Verstappen" },
+      { id: "2", name: "Carlos Sainz Jr." },
+    ],
+  });
+  await interceptGql(page, "GetDegreesOfSeparation", {
+    degreesOfSeparation: [
+      {
+        driver1: { id: "1", name: "Max Verstappen" },
+        driver2: { id: "2", name: "Carlos Sainz Jr." },
+        dates: [
+          {
+            startRace: "start",
+            endRace: "end",
+            startDate: "2015",
+            endDate: "2016",
+          },
+        ],
+      },
+    ],
+  });
+  await page.goto("http://localhost:5173/");
+
+  // Act
+  // Only select the second driver - The first is selected by default.
+  await page.locator("#driver2").fill("Carlos Sainz Jr.");
+  await page.locator("#driver2-option-0").click();
+
+  // await expect(page.locator("#degreesOfSeparation")).toContainText(
+  //   "1 degree of separation:0) Max Verstappen1) Carlo Sainz Jr."
+  // );
+
+  // Assert
+  await expect(page.locator("#driver-1-accordion-summary")).toHaveText(
+    "0) Max Verstappen"
+  );
+  await expect(page.locator("#driver-2-accordion-summary")).toHaveText(
+    "1) Carlos Sainz Jr."
+  );
+  await expect(page.locator("#driver-2-accordion-details")).toHaveText(
+    "Max Verstappen was teammates with Carlos Sainz Jr.from: 2015 startto:2016 end"
+  );
+});
+
+test("can see multple links", async ({ page }) => {
+  // Arrange
+  await interceptGql(page, "GetDrivers", {
+    drivers: [
+      { id: "1", name: "Max Verstappen" },
+      { id: "2", name: "Carlos Sainz Jr." },
+      { id: "3", name: "Charles Leclerc" },
+    ],
+  });
+  await interceptGql(page, "GetDegreesOfSeparation", {
+    degreesOfSeparation: [
+      {
+        driver1: { id: "1", name: "Max Verstappen" },
+        driver2: { id: "2", name: "Carlos Sainz Jr." },
+        dates: [
+          {
+            startRace: "start",
+            endRace: "end",
+            startDate: "2015",
+            endDate: "2016",
+          },
+        ],
+      },
+      {
+        driver1: { id: "2", name: "Carlos Sainz Jr." },
+        driver2: { id: "3", name: "Charles Leclerc" },
+        dates: [
+          {
+            startRace: "start",
+            endRace: "end",
+            startDate: "1234",
+            endDate: "4321",
+          },
+        ],
+      },
+    ],
+  });
+  await page.goto("http://localhost:5173/");
+
+  // Act
+  // Only select the second driver - The first is selected by default.
+  await page.locator("#driver2").fill("Charles Leclerc");
+  await page.locator("#driver2-option-0").click();
+
+  // await expect(page.locator("#degreesOfSeparation")).toContainText(
+  //   "1 degree of separation:0) Max Verstappen1) Carlo Sainz Jr."
+  // );
+
+  // Assert
+  await expect(page.locator("#driver-1-accordion-summary")).toHaveText(
+    "0) Max Verstappen"
+  );
+  await expect(page.locator("#driver-2-accordion-summary")).toHaveText(
+    "1) Carlos Sainz Jr."
+  );
+  await expect(page.locator("#driver-3-accordion-summary")).toHaveText(
+    "2) Charles Leclerc"
+  );
+  await expect(page.locator("#driver-2-accordion-details")).toHaveText(
+    "Max Verstappen was teammates with Carlos Sainz Jr.from: 2015 startto:2016 end"
+  );
+  await expect(page.locator("#driver-3-accordion-details")).toHaveText(
+    "Carlos Sainz Jr. was teammates with Charles Leclercfrom: 1234 startto:4321 end"
+  );
+});
+
+test("can see multiple race ranges", async ({ page }) => {
+  // Arrange
+  await interceptGql(page, "GetDrivers", {
+    drivers: [
+      { id: "1", name: "Max Verstappen" },
+      { id: "2", name: "Carlos Sainz Jr." },
+    ],
+  });
+  await interceptGql(page, "GetDegreesOfSeparation", {
+    degreesOfSeparation: [
+      {
+        driver1: { id: "1", name: "Max Verstappen" },
+        driver2: { id: "2", name: "Carlos Sainz Jr." },
+        dates: [
+          {
+            startRace: "start1",
+            endRace: "end1",
+            startDate: "1234",
+            endDate: "4321",
+          },
+          {
+            startRace: "start2",
+            endRace: "end2",
+            startDate: "1235",
+            endDate: "5321",
+          },
+        ],
+      },
+    ],
+  });
+  await page.goto("http://localhost:5173/");
+
+  // Act
+  // Only select the second driver - The first is selected by default.
+  await page.locator("#driver2").fill("Carlos Sainz Jr.");
+  await page.locator("#driver2-option-0").click();
+
+  // await expect(page.locator("#degreesOfSeparation")).toContainText(
+  //   "1 degree of separation:0) Max Verstappen1) Carlo Sainz Jr."
+  // );
+
+  // Assert
+  await expect(page.locator("#driver-1-accordion-summary")).toHaveText(
+    "0) Max Verstappen"
+  );
+  await expect(page.locator("#driver-2-accordion-summary")).toHaveText(
+    "1) Carlos Sainz Jr."
+  );
+  await expect(page.locator("#driver-2-accordion-details")).toHaveText(
+    "Max Verstappen was teammates with Carlos Sainz Jr.from: 1234 start1to:4321 end1from: 1235 start2to:5321 end2"
+  );
+});
