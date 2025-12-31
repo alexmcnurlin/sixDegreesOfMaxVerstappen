2420f5c4150b4866b77ce0c4922d596f0e25f63c
Alex McNurlin <alemcnurlin@gmail.com>
2025-12-30 19:56:04 -0700

    Build issues

--- postbuild.sh
+++ postbuild.sh
@@ -3,6 +3,7 @@
 rm -rf ./.amplify-hosting
  
 mkdir -p ./.amplify-hosting/compute
+mkdir -p ./.amplify-hosting/static
  
 cp -vr ./server/lib/ ./.amplify-hosting/compute/default
 cp -r ./server/node_modules ./.amplify-hosting/compute/default
